531661fbe4d984ce6f05574050d29db1557825dd
zh <zh@qq.com>
2025-06-20 17:03:43 +0800

    优化代码

--- common/src/main/java/com/yuanquan/common/widget/color_picker/ColorSelectAdapter.java
+++ common/src/main/java/com/yuanquan/common/widget/color_picker/ColorSelectAdapter.java
@@ -1,10 +1,12 @@
 package com.yuanquan.common.widget.color_picker;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -20,6 +22,7 @@ public class ColorSelectAdapter extends RecyclerView.Adapter<ColorSelectAdapter.
     private List<String> colors;
     private String curColor;
     private OnColorClickListener onColorClickListener;
+    private Context context;
 
     public ColorSelectAdapter(List<String> colors) {
         this.colors = colors;
@@ -28,6 +31,7 @@ public class ColorSelectAdapter extends RecyclerView.Adapter<ColorSelectAdapter.
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context = parent.getContext();
         View root = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_color_select, parent, false);
         return new ViewHolder(root);
     }
@@ -37,9 +41,17 @@ public class ColorSelectAdapter extends RecyclerView.Adapter<ColorSelectAdapter.
         String color1 = colors.get(position);
         int color = Color.parseColor(color1);
         viewHolder.colorDisplay.setBackgroundColor(color);
-        viewHolder.colorDisplay.setSelected(color1.equals(curColor));
+        if (color1.equals(curColor)) {
+//            viewHolder.colorDisplay.setSelected(true);
+            viewHolder.rl_color.setBackground(context.getResources().getDrawable(R.drawable.shape_bg_r4_green_primary));
+        } else {
+//            viewHolder.colorDisplay.setSelected(false);
+            viewHolder.rl_color.setBackground(context.getResources().getDrawable(R.drawable.shape_bg_r4_eae9e5));
+        }
         viewHolder.itemView.setOnClickListener(v -> {
             if (onColorClickListener != null) {
+                curColor = color1;
+                notifyDataSetChanged();
                 onColorClickListener.onColorClick(color1);
             }
         });
@@ -65,10 +77,12 @@ public class ColorSelectAdapter extends RecyclerView.Adapter<ColorSelectAdapter.
 
     static class ViewHolder extends RecyclerView.ViewHolder {
         public ImageView colorDisplay;
+        public RelativeLayout rl_color;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             colorDisplay = itemView.findViewById(R.id.color_display);
+            rl_color = itemView.findViewById(R.id.rl_color);
         }
     }
 }
