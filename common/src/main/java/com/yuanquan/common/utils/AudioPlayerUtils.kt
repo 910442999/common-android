744877f378b28d2a1e96b6713790185d65d7f9bf
zh <zh@qq.com>
2025-06-27 18:02:25 +0800

    增加录音，和音频播放

--- common/src/main/java/com/yuanquan/common/utils/AudioPlayerUtils.kt
+++ common/src/main/java/com/yuanquan/common/utils/AudioPlayerUtils.kt
@@ -1,10 +1,9 @@
-package com.oook.lib.utils
+package com.yuanquan.common.utils
 
 import android.media.AudioAttributes
 import android.media.AudioFormat
 import android.media.AudioTrack
 import android.os.Build
-import com.yuanquan.common.utils.LogUtil
 import java.util.concurrent.Executors
 import java.util.concurrent.LinkedBlockingQueue
 import java.util.concurrent.TimeUnit
