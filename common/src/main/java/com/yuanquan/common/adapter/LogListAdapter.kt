e9a44b83b8cc8e61e902eadfb53e9f6d907d1f10
zh <zh@qq.com>
2025-12-09 20:57:37 +0800

    优化日志相关

--- common/src/main/java/com/yuanquan/common/adapter/LogListAdapter.kt
+++ common/src/main/java/com/yuanquan/common/adapter/LogListAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.chad.library.adapter4.BaseQuickAdapter
+import com.yuanquan.common.R
 import com.yuanquan.common.databinding.LayoutLogListItemBinding
 
 class LogListAdapter : BaseQuickAdapter<String, LogListAdapter.VH>() {
@@ -18,7 +19,13 @@ class LogListAdapter : BaseQuickAdapter<String, LogListAdapter.VH>() {
     override fun onBindViewHolder(holder: VH, position: Int, item: String?) {
         if (item == null) return
         with(item) {
-            holder.binding.tvFileType.text = item
+            if (item.contains("[ERROR]")) {
+                holder.binding.tvFileType.text = item
+                holder.binding.tvFileType.setTextColor(context.resources.getColor(R.color.red))
+            } else {
+                holder.binding.tvFileType.text = item
+                holder.binding.tvFileType.setTextColor(context.resources.getColor(R.color.gray_333333))
+            }
         }
 
     }
