12673a11ea1533635d83800d908f1697621dadc0
zh <zh@qq.com>
2026-06-11 09:47:50 +0800

    合并代码

--- common/src/main/java/com/yuanquan/common/utils/PickerUtils.java
+++ common/src/main/java/com/yuanquan/common/utils/PickerUtils.java
@@ -2,7 +2,13 @@ package com.yuanquan.common.utils;
 
 import android.content.Context;
 import android.content.pm.ActivityInfo;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.bigkoo.pickerview.builder.OptionsPickerBuilder;
 import com.bigkoo.pickerview.builder.TimePickerBuilder;
@@ -73,6 +79,95 @@ public class PickerUtils {
                 .build();
         mPickerView.setPicker(optionsItems);
         mPickerView.show();
+        return mPickerView;
+    }
+
+    public static <T> OptionsPickerView showOptionsPicker(Context context, String textContentConfirm, String textContentCancel, int option1, List<T> optionsItems, int res, CustomListener listener, ViewGroup decorView, final OnOptionSelectListener onSelectListener) {
+        OptionsPickerBuilder builder = new OptionsPickerBuilder(context, new OnOptionsSelectListener() {
+            @Override
+            public void onOptionsSelect(int options1, int option2, int options3, View v) {
+                if (onSelectListener != null)
+                    onSelectListener.onOptionsSelect(options1, option2, options3, v);
+            }
+        })
+                .setSubmitColor(0xFFb5d260)//确定按钮文字颜色
+                .setCancelColor(0xFFb5d260)//取消按钮文字颜色
+                .setTitleBgColor(0xFFFFFFFF)//标题背景颜色 Night mode
+                .setContentTextSize(18)//滚轮文字大小
+                .setLineSpacingMultiplier(2)//滚轮文字大小
+                .setSubmitText(textContentConfirm)
+                .setCancelText(textContentCancel).
+                setLayoutRes(res, listener)
+                .setSelectOptions(option1);
+        if (decorView != null) {
+            builder.setDecorView(decorView);
+        }
+        OptionsPickerView<T> mPickerView = builder.build();
+        mPickerView.setPicker(optionsItems);
+        mPickerView.show();
+        return mPickerView;
+    }
+
+    public static <T> OptionsPickerView showOptionsPicker(Context context, String textContentConfirm, String textContentCancel, int option1, List<T> optionsItems, int res, CustomListener listener, boolean isDialog, final OnOptionSelectListener onSelectListener) {
+        OptionsPickerBuilder builder = new OptionsPickerBuilder(context, new OnOptionsSelectListener() {
+            @Override
+            public void onOptionsSelect(int options1, int option2, int options3, View v) {
+                if (onSelectListener != null)
+                    onSelectListener.onOptionsSelect(options1, option2, options3, v);
+            }
+        })
+                .setSubmitColor(0xFFb5d260)//确定按钮文字颜色
+                .setCancelColor(0xFFb5d260)//取消按钮文字颜色
+                .setTitleBgColor(0xFFFFFFFF)//标题背景颜色 Night mode
+                .setContentTextSize(18)//滚轮文字大小
+                .setLineSpacingMultiplier(2)//滚轮文字大小
+                .setSubmitText(textContentConfirm)
+                .setCancelText(textContentCancel).
+                setLayoutRes(res, listener)
+                .setSelectOptions(option1)
+                .isDialog(isDialog);
+        OptionsPickerView<T> mPickerView = builder.build();
+        mPickerView.setPicker(optionsItems);
+        if (isDialog) {
+            ViewGroup dialogContainerLayout = mPickerView.getDialogContainerLayout();
+            ViewGroup.LayoutParams layoutParams = dialogContainerLayout.getLayoutParams();
+            layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT;
+            if (layoutParams instanceof ViewGroup.MarginLayoutParams) {
+                ViewGroup.MarginLayoutParams marginLayoutParams = (ViewGroup.MarginLayoutParams) layoutParams;
+                marginLayoutParams.leftMargin = 0;
+                marginLayoutParams.rightMargin = 0;
+            }
+            dialogContainerLayout.setLayoutParams(layoutParams);
+            if (dialogContainerLayout.getChildCount() > 0) {
+                View childView = dialogContainerLayout.getChildAt(0);
+                ViewGroup.LayoutParams childLayoutParams = childView.getLayoutParams();
+                childLayoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT;
+                childView.setLayoutParams(childLayoutParams);
+            }
+        }
+        if (isDialog && mPickerView.getDialog() != null) {
+            Window window = mPickerView.getDialog().getWindow();
+            if (window != null) {
+                window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                window.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+                window.setGravity(Gravity.BOTTOM);
+                window.setWindowAnimations(com.bigkoo.pickerview.R.style.picker_view_slide_anim);
+            }
+        }
+        mPickerView.show();
+        if (isDialog && mPickerView.getDialog() != null) {
+            Window window = mPickerView.getDialog().getWindow();
+            if (window != null) {
+                window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                window.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+                window.setGravity(Gravity.BOTTOM);
+                window.setWindowAnimations(com.bigkoo.pickerview.R.style.picker_view_slide_anim);
+                window.setLayout(
+                        WindowManager.LayoutParams.MATCH_PARENT,
+                        WindowManager.LayoutParams.WRAP_CONTENT
+                );
+            }
+        }
         return mPickerView;
     }
 
